8f9caafe2a3117107d9eabe3c60fccdb94ea110a
BuildTools <justinkrasner98@gmail.com>
2021-01-28 11:12:02 -0500

    added more words, all work

--- src/services/randomWords.js
+++ src/services/randomWords.js
@@ -1,7 +1,26 @@
 let wordList = [
-  "test"
+  "hi",
+  "artichokes",
+  "lumberjack",
+  "coding",
+  "world",
+  "binoculars",
+  "cornflakes",
+  "education",
+  "notebook",
+  "blacksmith",
+  "animal",
+  "wood",
+  "programming",
+  "love",
+  "building",
+  "zebra",
+  "snowman",
+  "winter",
+  "scarf",
+  "carrot"
 ]
 
-export default function randomWords() {
-  return wordList[Math.floor(Math.random() * wordList.length)];
+export default function randomWords(){
+  return wordList[Math.floor(Math.random()*wordList.length)];
 }
